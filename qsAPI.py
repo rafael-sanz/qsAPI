a414070350e7b4953ce79426e47f620b1e2d7a2d
gammar07 <marcin.gamza@gmail.com>
2018-02-28 14:14:39 +0100

    Fixed Invalid header name error

--- qsAPI.py
+++ qsAPI.py
@@ -4,16 +4,12 @@
 @author:     Rafael Sanz
 @contact:    rafael.sanz@selab.es
 @Copyright 2016 <Rafael Sanz - (R)SELAB>
-
 This software is MIT licensed (see terms below)
-
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
     files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
     modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
     Software is furnished to do so, subject to the following conditions:
-
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
     LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
@@ -140,7 +136,7 @@ class _Controller(object):
         hd= { 'User-agent': self._referer,
               'Pragma': 'no-cache',
               'X-Qlik-User': 'UserDirectory={directory}; UserId={user}'.format(directory=self.UserDirectory, user=self.UserId),
-              'X-Qlik-Virtual-Proxy-Prefix:': self.vproxy,
+              'X-Qlik-Virtual-Proxy-Prefix': self.vproxy,
               'x-Qlik-Xrfkey': par.get('Xrfkey'),
               'Accept': 'application/json',
               'Content-Type': 'application/json'}
@@ -656,5 +652,3 @@ if __name__ == "__main__":
     pprint(qrs.AppDictAttributes())
     pprint([qrs.count(x) for x in ('app','user','stream','dataconnection')])
 
-
-    
